3f6770746aac0e45c58bbbd35770a046d4d59c2f
alex hay <alexanderhay2020@u.northwestern.edu>
2019-09-16 17:31:34 -0500

    input test

--- servo.py
+++ servo.py
@@ -1,1 +1,12 @@
 # Servo controlled camera for hackathon
+
+import serial
+
+ser=serial.Serial('/dev/ttyACM0',baudrate=9600)
+
+print 'enter angle between 0 and 90'
+input=raw_input()
+
+if input<0 or input>90:
+    print 're-enter angle between 0 and 90'
+    input=raw_input()

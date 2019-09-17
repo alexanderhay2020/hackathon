cd397ef47440ddf465f132333a092fb49d72c350
alex hay <alexanderhay2020@u.northwestern.edu>
2019-09-17 15:22:38 -0500

    first commit before changing to rgb

--- camera.py
+++ camera.py
@@ -8,13 +8,17 @@ def nothing(x):
 
 cap = cv2.VideoCapture(2) # reads from 2nd camera (cv2.VideoCapture(0) is the webcam)
 
-# creates window named "HSV"
+# creates windows named "Stock" and "HSV"
+cv2.namedWindow('Stock')
 cv2.namedWindow('HSV')
 
-# creates 3 trackbars in window HSV
-cv2.createTrackbar('H','HSV',0,255,nothing) # (trackbar caption, parent window, preset val, max val,function called when trackbar moves)
-cv2.createTrackbar('S','HSV',0,255,nothing)
-cv2.createTrackbar('V','HSV',0,255,nothing)
+# creates 6 trackbars in window HSV, hi/low thresholds
+cv2.createTrackbar('hh','HSV',0,255,nothing) # (trackbar caption, parent window, preset val, max val,function called when trackbar moves)
+cv2.createTrackbar('hl','HSV',0,255,nothing)
+cv2.createTrackbar('sh','HSV',0,255,nothing)
+cv2.createTrackbar('sl','HSV',0,255,nothing)
+cv2.createTrackbar('vh','HSV',0,255,nothing)
+cv2.createTrackbar('vl','HSV',0,255,nothing)
 
 while True: # starts loop to run camera
 
@@ -22,19 +26,26 @@ while True: # starts loop to run camera
     ret, frame = cap.read() # returns value of the feed, the image itself
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # converts feed to grayscale
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # converts feed to grayscale
     hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV) # converts feed to HSV
+
+    # get current positions of four trackbars
+    hl = cv2.getTrackbarPos('hl','HSV') # name of trackbar, name of parent window
+    hh = cv2.getTrackbarPos('hh','HSV')
+    sl = cv2.getTrackbarPos('sl','HSV')
+    sh = cv2.getTrackbarPos('sh','HSV')
+    vl = cv2.getTrackbarPos('vl','HSV')
+    vh = cv2.getTrackbarPos('vh','HSV')
+
+    hsvl = np.array([hl,sl,vl])
+    hsvh = np.array([hh,sh,vh])
+
+    mask = cv2.inRange(hsv, hsvl, hsvh)
 
     # Display the resulting frame
     #cv2.imshow('frame1',frame) # displays raw feed
-    #cv2.imshow('frame2',gray) # displays grayscale feed
-    cv2.imshow('HSV',hsv) # displays HSV feed
-
-
-    # get current positions of four trackbars
-    h = cv2.getTrackbarPos('H','HSV') # name of trackbar, name of parent window
-    s = cv2.getTrackbarPos('S','HSV')
-    v = cv2.getTrackbarPos('V','HSV')
+    cv2.imshow('Stock',hsv) # displays stock HSV feed
+    cv2.imshow('HSV',mask) # displays modified HSV feed
 
     if cv2.waitKey(1) & 0xFF == ord('q'): # quits program if 'q' is pressed
         break

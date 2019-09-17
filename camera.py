471b10a4e2d01419ace8fabbca03b7c054c081bb
alex hay <alexanderhay2020@u.northwestern.edu>
2019-09-17 15:33:58 -0500

    RGB filter with mask works

--- camera.py
+++ camera.py
@@ -10,15 +10,15 @@ cap = cv2.VideoCapture(2) # reads from 2nd camera (cv2.VideoCapture(0) is the we
 
 # creates windows named "Stock" and "HSV"
 cv2.namedWindow('Stock')
-cv2.namedWindow('HSV')
+cv2.namedWindow('RGB')
 
 # creates 6 trackbars in window HSV, hi/low thresholds
-cv2.createTrackbar('hh','HSV',0,255,nothing) # (trackbar caption, parent window, preset val, max val,function called when trackbar moves)
-cv2.createTrackbar('hl','HSV',0,255,nothing)
-cv2.createTrackbar('sh','HSV',0,255,nothing)
-cv2.createTrackbar('sl','HSV',0,255,nothing)
-cv2.createTrackbar('vh','HSV',0,255,nothing)
-cv2.createTrackbar('vl','HSV',0,255,nothing)
+cv2.createTrackbar('rl','RGB',0,255,nothing) # (trackbar caption, parent window, preset val, max val,function called when trackbar moves)
+cv2.createTrackbar('rh','RGB',0,255,nothing)
+cv2.createTrackbar('gl','RGB',0,255,nothing)
+cv2.createTrackbar('gh','RGB',0,255,nothing)
+cv2.createTrackbar('bl','RGB',0,255,nothing)
+cv2.createTrackbar('bh','RGB',0,255,nothing)
 
 while True: # starts loop to run camera
 
@@ -27,25 +27,25 @@ while True: # starts loop to run camera
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # converts feed to grayscale
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV) # converts feed to HSV
+    rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) # converts feed to RGB
 
     # get current positions of four trackbars
-    hl = cv2.getTrackbarPos('hl','HSV') # name of trackbar, name of parent window
-    hh = cv2.getTrackbarPos('hh','HSV')
-    sl = cv2.getTrackbarPos('sl','HSV')
-    sh = cv2.getTrackbarPos('sh','HSV')
-    vl = cv2.getTrackbarPos('vl','HSV')
-    vh = cv2.getTrackbarPos('vh','HSV')
+    rl = cv2.getTrackbarPos('rl','RGB') # name of trackbar, name of parent window
+    rh = cv2.getTrackbarPos('rh','RGB')
+    gl = cv2.getTrackbarPos('gl','RGB')
+    gh = cv2.getTrackbarPos('gh','RGB')
+    bl = cv2.getTrackbarPos('bl','RGB')
+    bh = cv2.getTrackbarPos('bh','RGB')
 
-    hsvl = np.array([hl,sl,vl])
-    hsvh = np.array([hh,sh,vh])
+    rgbl = np.array([rl,gl,bl])
+    rgbh = np.array([rh,gh,bh])
 
-    mask = cv2.inRange(hsv, hsvl, hsvh)
+    mask = cv2.inRange(rgb, rgbl, rgbh)
 
     # Display the resulting frame
     #cv2.imshow('frame1',frame) # displays raw feed
-    cv2.imshow('Stock',hsv) # displays stock HSV feed
-    cv2.imshow('HSV',mask) # displays modified HSV feed
+    cv2.imshow('Stock',frame) # displays stock HSV feed
+    cv2.imshow('RGB',mask) # displays modified HSV feed
 
     if cv2.waitKey(1) & 0xFF == ord('q'): # quits program if 'q' is pressed
         break

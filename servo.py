e3043f6ad0a4706d52192fc71d4461a45462fe99
alex hay <alexanderhay2020@u.northwestern.edu>
2019-09-17 11:10:25 -0500

    first commit before converting to arrow key input

--- servo.py
+++ servo.py
@@ -1,45 +1,40 @@
 # Servo controlled camera for hackathon
 
 import serial
+import time
 
-#ser=serial.Serial('/dev/ttyACM0',baudrate=9600)
+ser=serial.Serial('/dev/ttyACM1',baudrate=9600)
+input=[0.0,22.5,45.0,67.5,90.0]
+output=[None]*5
+for i in range(5):
+    print 'enter angle between 0 and 90'
+    #input=float(raw_input())
 
-print 'enter angle between 0 and 90'
-input=float(raw_input())
+    # check if input is valid
+    #while input<0 or input>90 or input==int:
+    #            print 're-enter angle between 0 and 90'
+    #        input=raw_input()
 
-# check if input is valid
-#while input<0 or input>90 or input==int:
-#            print 're-enter angle between 0 and 90'
-#        input=raw_input()
+    # 992 - 1500 - 2000 . . . shortest - middle - longest in us
+    print input[i]
+    output2 = int(((1008*(input[i]/90))+992)*4) # 2000-992=100
+    print output2
+    output[i]=output2
+    # input/90 gives us a percentage
+    # (percentage*the range) + the minimum of the range gives us a final spot
+    # *4 because we're operating in quarter microseconds (us)
 
-# 992 - 1500 - 2000 . . . shortest - middle - longest in us
+    i=i+1
+    #ser.write('\x84\x00'+chr(output & 127) + chr((output & 16256) >> 7))
 
-output = ((1008*(input/90))+992)*4 # 2000-992=1008
+    # compare output with a number that in binary is all 1's and all 0's
+    # 127 in binary is 1111111
+    # 16256 in binary is 11111110000000
+    # comparing the latter you'll end up with a number xxxxxxxyyyyyyy. >> 7 shifts that to xxxxxxx
+    # using chr() on those numbers outputs a string for each. add those to the servo\channel string
+    # '\' add themselves into the string
 
-# input/90 gives us a percentage
-# (percentage*the range) + the minimum of the range gives us a final spot
-# *4 because we're operating in quarter microseconds (us)
-
-
-#lowerbits='0'+'str(bin(int(output)))[-7:]
-bits=str(bin(int(output)))[2:]
-lowerbits='0'+bits[-7:]
-# int() to give bin() a proper argument
-# convert bin to a string
-# call for the last 7 digits
-# insert a '0' at the beginning of the string
-# lowerbits='0'+(str(bin(int(output)))[2:])[-7:] # does it all in one line
-
-upperbits=bits[:(len(bits)-7)] # retrieves the first bits not called from lower lowerbits
-
-hexlb=hex(int(lowerbits,2))[1:] # produces a string of the hex value to pass to .write
-hexub=hex(int(upperbits,2))[1:] #same thing
-
-for prefix in range(8-len(upperbits)):
-    upperbits='0'+upperbits
-
-    # adds 0's to upperbits to prep for hex conversion
-
-print output
-
-ser.write('\x84\x00\x40\x3E')
+for i2 in range(5):
+    ser.write('\x84\x00'+chr(output[i2] & 127) + chr((output[i2] & 16256) >> 7))
+    ser.write('\x84\x01'+chr(output[i2] & 127) + chr((output[i2] & 16256) >> 7))
+    time.sleep(0.5)
